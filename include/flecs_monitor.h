110911db96803ab552503d995e4179159ab36155
Arn Carveris <arncis96@gmail.com>
2021-01-24 00:08:14 +0200

    added missing `FLECS_LEGACY` check

--- include/flecs_monitor.h
+++ include/flecs_monitor.h
@@ -4,9 +4,13 @@
 /* This generated file contains includes for project dependencies */
 #include "flecs-monitor/bake_config.h"
 
+#ifndef FLECS_LEGACY
+
 ECS_STRUCT(EcsMonitorServer, {
     int16_t port;
 });
+
+#endif
 
 #ifdef __cplusplus
 extern "C" {
